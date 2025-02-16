2309ed80d01a98ab76b2239a24f44df6397881e1
KamilleDaoust <kamilledaoust032007@outlook.com>
2025-02-16 11:44:57 -0500

    Création script

--- Assets/Scripts/DemarrerParcours.cs
+++ Assets/Scripts/DemarrerParcours.cs
@@ -7,7 +7,7 @@ public class DemarrerParcours : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hiii");
     }
 
     // Update is called once per frame
